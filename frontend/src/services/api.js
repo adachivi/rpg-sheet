bd3ea02ebf029422c203675200f35813fcb77609
Victor Adachi <adachivi.laus@gmail.com>
2025-08-06 16:02:02 -0300

    Frontend deploy.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,9 @@ import axios from "axios"; // Lib. for HTTP requests (GET, POST, etc.)
 
 // Create an instance of axios integrated with the backend
 const api = axios.create({
-    baseURL: "https://rpg-sheet.onrender.com",
+
+    baseURL: import.meta.env.VITE_API_URL,
+
 });
 
 export default api; // Make it available to the rest of the project
--- frontend/src/utils/Utils.jsx
+++ frontend/src/utils/Utils.jsx
@@ -1,13 +1,8 @@
 // Utility file
 
-// Update a text value from a state
-export const handleTextInputChange = (key, event, setSheet) => {
-    const newValue = event.target.value; // Get the new value inputted
-    setSheet((prev) => ({...prev, [key]: newValue}));
-}
-
 // Updates the sheet's attribute value when a checkbox is clicked
 export const handleCheckboxClick = (attr, index, attrCheckboxes, setSheet) => {
+
     const checkboxes = [...attrCheckboxes[attr]];
     if (index == 0 && checkboxes[0] == true && checkboxes[1] != true) {
         setSheet((prev) => ({...prev, [attr]: index}));
@@ -15,10 +10,12 @@ export const handleCheckboxClick = (attr, index, attrCheckboxes, setSheet) => {
     else {
         setSheet((prev) => ({...prev, [attr]: (index + 1)}));
     }
+
 }
 
 // Checks the checkboxes according to it's attribute value
 export const handleCheckboxes = (attr, attrValue, attrCheckboxes, setAttrCheckboxes) => {
+
     const initializedAttrCheckboxes = [...attrCheckboxes[attr]];
     for (let i = 0; i < 5; i++) {
         if (i <= (attrValue - 1)) {
@@ -30,10 +27,20 @@ export const handleCheckboxes = (attr, attrValue, attrCheckboxes, setAttrCheckbo
     };
 
     setAttrCheckboxes((prev) => ({...prev, [attr]: initializedAttrCheckboxes}));
+
+}
+
+// Update a text value from a state
+export const handleTextInputChange = (key, event, setSheet) => {
+
+    const newValue = event.target.value; // Get the new value inputted
+    setSheet((prev) => ({...prev, [key]: newValue}));
+
 }
 
 // Verify if the player's name is valid: has at least 2 letters
 export const verifyPlayerNameInput = (playerName) => {
+
     let alphabeticalRegex = /[A-Za-z]{2,}/;
 
     if (alphabeticalRegex.test(playerName) == true) {
@@ -42,4 +49,5 @@ export const verifyPlayerNameInput = (playerName) => {
     else {
         return false;
     }
+
 }
